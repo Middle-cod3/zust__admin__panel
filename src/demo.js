28c57fd7fb47f1dc40762b3634f6664a33cf8464
Middle-cod3 <92102240+Middle-cod3@users.noreply.github.com>
2022-08-02 01:32:39 +0530

    v1

---
+++ src/demo.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class demo extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>demo</div>
+    )
+  }
+}
+
+export default demo
